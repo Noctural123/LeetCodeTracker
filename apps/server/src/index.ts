21b09860a6f25ba3f9082347350909591d184645
An Nguyen <hongan.nguyen04@gmail.com>
2025-11-04 01:59:41 -0600

    Set up express

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,17 @@
 import express from "express";
 import cors from "cors";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import { z } from "zod";import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" })
+});
+
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
