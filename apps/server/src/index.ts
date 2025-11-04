821cf1c5b4249ea12a1507b49516e7f9df89ee97
An Nguyen <hongan.nguyen04@gmail.com>
2025-11-04 02:59:40 -0600

    Finally understand nodejs

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { z } from "zod";import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Simple test for health check
 app.get("/health", (req, res) => {
     res.json({ status: "ok" })
 });
-//Test
+
+
 const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
